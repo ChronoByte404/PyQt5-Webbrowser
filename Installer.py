5472a17f438fe7159182167bea428ed07b9491d2
Jack <74297154+JackSAI-15@users.noreply.github.com>
2021-10-10 21:09:07 +0100

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,13 +1,13 @@
 import os
 
+print("Attempting path script.")
+
+os.system("pip install PyQt5")
+os.system("pip install PyQtwebEngine")
+
 print("Attempting universal installation scripts.")
 
 os.system("python3 -m pip install PyQt5")
 os.system("python3 -m pip install PyQtwebEngine")
-
-print("Attempting Windows script.")
-
-os.system("pip install PyQt5")
-os.system("pip install PyQtwebEngine")
 
 print("Done.")
